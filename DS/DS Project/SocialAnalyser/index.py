0f6d4c3c27195704f4f489fd3695ad564c8acc71
VarunSingh19 <varunsingh18@icloud.com>
2024-01-17 15:41:44 +0530

    adding

--- DS/DS Project/SocialAnalyser/index.py
+++ DS/DS Project/SocialAnalyser/index.py
@@ -1,11 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+url = "https://www.statista.com/statistics/272014/global-social-networks-ranked-by-number-of-users/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+user_counts = []
+social_networks = []
+table = soup.find('table')
+rows = table.find_all('tr')[1:]  
 
-social_media_platforms = ["Instagram", "Facebook", "Snapchat", "WhatsApp"]
-user_counts = [2000, 3049, 293, 2000] 
+for row in rows:
+    columns = row.find_all('td')
+    social_network = columns[0].text.strip()
+    users = int(columns[1].text.replace(',', '').strip())  
+    social_networks.append(social_network)
+    user_counts.append(users)
 
-plt.figure(figsize=(10, 6))
-plt.bar(social_media_platforms, user_counts, color=['blue', 'green', 'yellow', 'purple'])
-plt.title('Number of Users on Social Media Platforms')
-plt.xlabel('Social Media Platforms')
-plt.ylabel('Number of Users (in millions)')
+plt.figure(figsize=(12, 8))
+plt.barh(social_networks, user_counts, color='skyblue', edgecolor='black')
+plt.xlabel('Number of Users (in millions)', fontsize=14)
+plt.ylabel('Social Network', fontsize=14)
+plt.title('Social Networks Users Reach Analysis', fontsize=18, fontweight='bold')
+for index, value in enumerate(user_counts):
+    plt.text(value, index, f'{value:,}'+" millions", va='center', fontsize=12)
+
+plt.gca().invert_yaxis()
+plt.grid(axis='x', linestyle='--', alpha=0.6)
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
 plt.show()
+
--- DS/DS Project/StockPrice/index.py
+++ DS/DS Project/StockPrice/index.py
@@ -1,30 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# def get_stock_price(stock_symbol):
-#     url = f"https://finance.yahoo.com/quote/{stock_symbol}"
-#     response = requests.get(url)
-    # if response.status_code == 200:
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#         stock_price_element = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find('fin-streamer')
-#         if stock_price_element:
-#             stock_price = stock_price_element.text
-#             return f"The current stock price of {stock_symbol} is: {stock_price}"
-#         else:
-#             return f"Unable to find the stock price element for {stock_symbol}."
-#     else:
-#         return f"Failed to retrieve data for {stock_symbol}. Status code: {response.status_code}"
-# stocks = ["META", "AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "IBM", "META", "NFLX", "NVDA", "V", "PYPL", "GS", "JPM", "BA", "CSCO", "INTC", "DIS", "GE", "WMT"]
-# stock_data = []
-# for stock_symbol in stocks:
-#     stock_info = get_stock_price(stock_symbol)
-#     stock_data.append(stock_info)
-# with open('stock_data.txt', 'w') as file:
-#     for stock_info in stock_data:
-#         file.write(stock_info + '\n')
-# print("All stock data has been saved to stock_data.txt")
-
-
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
@@ -45,26 +18,18 @@ def get_stock_price(stock_symbol):
 
 stocks = ["META", "AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "IBM", "META", "NFLX", "NVDA", "V", "PYPL", "GS", "JPM", "BA", "CSCO", "INTC", "DIS", "GE", "WMT"]
 stock_data = []
-
 for stock_symbol in stocks:
     stock_info = get_stock_price(stock_symbol)
     stock_data.append(stock_info)
 
-# Open a text file for writing
 with open('stock_data.txt', 'w') as file:
-    # Write all stock information to the file
     for stock_info in stock_data:
         file.write(stock_info + '\n')
-
-# Extract stock prices from the information
 prices = [float(info.split(":")[-1].strip().replace(',', '').replace(' USD', '')) for info in stock_data]
-
-# Plotting the data
 plt.figure(figsize=(10, 6))
 plt.bar(stocks, prices, color='blue')
 plt.title('Stock Prices')
 plt.xlabel('Stock Symbols')
 plt.ylabel('Stock Rate')
 plt.show()
-
 print("All stock data has been saved to stock_data.txt, and the graph has been displayed.")
