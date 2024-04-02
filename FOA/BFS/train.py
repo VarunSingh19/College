47eb97da84457c08b0b0c03ce38299b8ca7b55fc
VarunSingh19 <varunsingh18@icloud.com>
2024-04-02 09:47:40 +0530

    Adding FOA practical PDF

--- FOA/BFS/train.py
+++ FOA/BFS/train.py
@@ -90,25 +90,27 @@
 
 
 graph = {
-  '5' : ['3','7'],
-  '3' : ['2', '4'],
-  '7' : ['8'],
-  '2' : [],
-  '4' : ['8'],
-  '8' : []
+    '1': ['3','7'],
+    '3': ['2','4'],
+    '7':['8'],
+    '2':[],
+    '4':['8'],
+    '8':[]
 }
 
-visited = [] 
-queue = []   
-def BFS_warmup(visited,graph,node):
+visited = []
+queue = []
+
+def bfs(visited,graph,node):
     visited.append(node)
     queue.append(node)
+
     while queue:
         m = queue.pop(0)
-        print(m, end= " ")
-        for neighbour in graph[m]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
-print("Following is the Breadth-First Search")
-BFS_warmup(visited, graph, '5')
+        print(m , end=" ")
+
+        for neighbor in graph [m]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+bfs(visited,graph,'1')
